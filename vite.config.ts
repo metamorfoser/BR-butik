be69af7eb988c00e7cc58ced1ec64c2d169f5214
metamorfoser <malin4690@gmail.com>
2026-02-09 10:41:16 +0100

    Add path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: "/BR-butik/", //för github
   server: {
     open: true,   //  öppnar webbläsaren automatiskt
     port: 5173    //  ändra port här
   },
   plugins: [react()],
 })
+
